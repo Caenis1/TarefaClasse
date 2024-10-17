981a08232fe9a067a959d9ca28e7f2b81a74c501
Caenis1 <carlossocial1479@gmail.com>
2024-10-17 19:32:39 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.somar(10, 20);
+        System.out.println("A soma é: " + resultado);
+    }
     }
 }
